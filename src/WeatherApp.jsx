a7a4606c0bf72053aab848698426d1a9880ac66d
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-02-05 18:33:08 +0100

    Updated

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
+
 export const WeatherApp = () => {
+
+     const [[ciudad, setCiudad] = useState("")
+
+     const handleCambioCiudad = (e) => {
+          setCiudad(e.target.value);
+}
+
   return (
     <div className="container">
-
       <h1>Weather App</h1>
 
+      <form>
+        <input type="text" 
+        value={ciudad} 
+        onChange={handleCambioCiudad} 
+        />
+        <button type="submit">Buscar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
