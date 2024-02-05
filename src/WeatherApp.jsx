00318d8f2820787faabd05d5285bd2b8773b0c78
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-02-05 18:47:33 +0100

    Updated

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
 
 export const WeatherApp = () => {
+  const [ciudad, setCiudad] = useState("");
 
-     const [[ciudad, setCiudad] = useState("")
-
-     const handleCambioCiudad = (e) => {
-          setCiudad(e.target.value);
-}
+  const handleCambioCiudad = (e) => {
+    setCiudad(e.target.value);
+  }
 
   return (
     <div className="container">
       <h1>Weather App</h1>
 
       <form>
-        <input type="text" 
-        value={ciudad} 
-        onChange={handleCambioCiudad} 
+        <input
+          type="text"
+          value={ciudad}
+          onChange={handleCambioCiudad}
         />
         <button type="submit">Buscar</button>
       </form>
